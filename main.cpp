4c6b14dd455a9688911ca75a0c891af90dc5279f
Bogdan <flomaster3008@gmail.com>
2023-04-11 18:44:47 +0300

    3DCube works

--- main.cpp
+++ main.cpp
@@ -12,24 +12,35 @@ using std::cout, std::endl, std::vector;
 
 
 vector<int> matrix_mult(const vector<vector<double>>& A, const vector<int>& B){
-    cout << A.size() << " " << A[0].size() << " " << B.size();
+    //cout << A.size() << " " << A[0].size() << " " << B.size();
+    //return vector<int>();
     vector<int> C(B.size());
-    for(int i = 0; i < B.size(); ++i){
-        C[i] = B[0] * A[i][0] + B[1] * A[i][1] + B[2] * A[i][2] + B[3] * A[i][2];
+    for(int i = 0; i < A.size(); ++i){
+        C[i] = B[0] * A[i][0] + B[1] * A[i][1]  + B[2] * A[i][2] ;
     }
     return C;
 }
 
 vector<vector<int>> make_cube(int r){
-    vector<vector<int>> Points = {{r, r, 0}, {r, -r, 0}, {-r, -r, 0}, {-r, -r, 0},
-                                  {r, r, r}, {r, -r, r}, {-r, -r, r}, {-r, -r, r} };
+    return {{r, -r, r}, {r, r, r}, {-r, r, r}, {-r, -r, r},
+            {-r, -r, -r}, {-r, r, -r}, {r, r, -r}, {r, -r, -r} };
     //cout << "Points size "<< Points.size() << " " << Points[0].size();
-    return Points;
+
+}
+
+vector<int> mat_sum(vector<int> a, vector<int> b, vector<int> c){
+    if(a.size() != b.size() || a.size() != c.size()){
+        throw std::runtime_error("different size of vec in vec_sum");
+    }
+    vector<int> d(a.size());
+    for(int i = 0; i < a.size(); ++i)
+        d[i] = a[i] + b[i] + c[i];
+    return d;
 }
 
 void draw_cube(cv::Mat & m, int radius, const vector<int>& angles){
     if(angles.size() != 3){
-        cout << "incorrect size of transformation matrix" << angles.size() << "\n";
+        cout << "incorrect size of angles matrix" << angles.size() << "\n";
         return;
     }
 
@@ -45,29 +56,37 @@ void draw_cube(cv::Mat & m, int radius, const vector<int>& angles){
     vector<vector<double>> y_transform = {{cos(degrees_y), 0, sin(degrees_y)},
                                   {0, 1, 0},
                                   {-1*sin(degrees_y), 0, cos(degrees_y)}};
-    vector<vector<double>> z_transform = {{cos(degrees_z), -1*sin(degrees_y), 0},
+    vector<vector<double>> z_transform = {{cos(degrees_z), -1*sin(degrees_z), 0},
                                   {sin(degrees_z), cos(degrees_z), 0},
                                   {0, 0, 1}};
 
     vector<vector<int>> cube_points = make_cube(radius);
 
-    cout << "cube_points size "<< cube_points.size();// << " " << cube_points[0].size();
     vector<vector<int>> mod_points(8);
 
     for(int i = 0; i < 8; i++){
-        matrix_mult(x_transform, cube_points[i]);
+        mod_points[i] = matrix_mult(x_transform, cube_points[i]);
+        mod_points[i] = matrix_mult(y_transform, mod_points[i]);
+        mod_points[i] = matrix_mult(z_transform, mod_points[i]);
     }
-return;
 
+
+    for(int i = 0; i < 8; i++) {
+
+        mod_points[i][0] += m.rows/2;
+        mod_points[i][1] += m.cols/2;
+
+    }
+    vector<cv::Scalar> colors = {{0,0,255}, {0,255,0}, {255,0,0}, {255, 255, 0}, {0,255,255}, {255,0,255}, {150,165,100}, {255,255,255}};
     for(int i = 1; i < 8; i++) {
-        if (mod_points[i][0] < m.rows && mod_points[i][1] < m.cols) {
-            m.at<uchar>(mod_points[i][0], mod_points[i][1]) = 255;
-            cv::line(m, {mod_points[i-1][0], mod_points[i-1][1],},
-                     {mod_points[i][0], mod_points[i][1],}, cv::Scalar(255),
-                 2, cv::LINE_8);
+        if (mod_points[i][0] > 0 && mod_points[i][1] > 0 && mod_points[i][0] < m.rows && mod_points[i][1] < m.cols) {
+            //m.at<uchar>(mod_points[i][0], mod_points[i][1]) = 255;
+            cv::Point p1(mod_points[i - 1][1], mod_points[i - 1][0]);
+            cv::Point p2(mod_points[i][1], mod_points[i][0]);
+            cv::line(m, p1, p2, colors[i], 5);
         }
-
     }
+
 
 }
 int main() {
@@ -82,19 +101,15 @@ int main() {
     cv::resizeWindow("track", y_size, 250);
     cv::moveWindow("track", 0, x_size + 0.05 * x_size);
 
-    cv::namedWindow("standart circle", 1);
-    cv::resizeWindow("standart circle", x_size, y_size);
-    cv::moveWindow("standart circle", 0, 0);
+    cv::namedWindow("cube", 1);
+    cv::resizeWindow("cube", x_size, y_size);
+    cv::moveWindow("cube", 0, 0);
 
-    cv::namedWindow("modified circle", 1);
-    cv::resizeWindow("modified circle", x_size, y_size);
-    cv::moveWindow("modified circle", y_size, 0);
+
 
     int radius = 50;
     cv::createTrackbar("Radius", "track", &radius, 200);
 
-    //Create trackbar to change contrast
-    //vector<double> transformation_matrix(4);
     vector<int> angles = {0, 0, 0};
 
     cv::createTrackbar("X", "track", &angles[0], 360);
@@ -106,8 +121,7 @@ int main() {
     cv::setTrackbarMin("Z", "track", -360);
 
     while(true) {
-        cv::Mat img_before = cv::Mat::zeros(x_size, y_size, CV_8UC1);
-        cv::Mat img_after = cv::Mat::zeros(x_size, y_size, CV_8UC1);
+        cv::Mat img = cv::Mat::zeros(x_size, y_size, CV_8UC3);
 
 
 
@@ -116,10 +130,9 @@ int main() {
         //draw_circle(img_after, 0, 0, radius, transformation_matrix);
 
         //cout << iSliderValue1 << " " << iSliderValue2 << "\n";
-        draw_cube(img_after, radius, angles);
+        draw_cube(img, radius, angles);
 
-        cv::imshow("standart circle", img_before);
-        cv::imshow("modified circle", img_after);
+        cv::imshow("cube", img);
 
         //cv::namedWindow("Trackbar Example");
 
